82db9c24f5144bd09ca3dd2df55952f34281b8b4
Catherine Price <catprice444@gmail.com>
2020-05-28 13:41:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,16 +12,27 @@ class Transfer
     @sender.valid? && @receiver.valid? == true
   end
 
+  # def execute_transaction
+  #   if sender.balance > @amount && @status == "pending"
+  #     @sender.balance -= @amount
+  #     @receiver.balance += @amount
+  #     @status = "complete"
+  #   else
+  #     @status = "rejected"
+  #     "Transaction rejected. Please check your account balance."
+  #   end
+  # end
+
   def execute_transaction
-    if sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
-    else
-      @status = "rejected"
-      "Transaction rejected. Please check your account balance."
-    end
+  if @sender.balance > @amount && @status == "pending"
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+    @status = "complete"
+  else
+    @status = "rejected"
+    return "Transaction rejected. Please check your account balance."
   end
+end
 
   def reverse_transfer
 
