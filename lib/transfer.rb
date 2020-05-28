cfb001a60edd6659e68c58e928b0ea0660b03601
Catherine Price <catprice444@gmail.com>
2020-05-28 12:51:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
   attr_accessor :sender, :receiver, :status, :transfer, :amount
 
-  def initialize(sender, receiver, transfer=nil)
+  def initialize(sender, receiver, transfer)
     @sender = sender
     @receiver = receiver
     @status = "pending"
