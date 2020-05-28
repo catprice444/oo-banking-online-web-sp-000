7a6d77b1c70f60db74bb790dee2c7777d93011a4
Catherine Price <catprice444@gmail.com>
2020-05-28 12:30:47 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,26 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    amount == deposit(self) + self.balance
+    @balance << amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+  end
+
+  def close_account
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,20 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :transfer
+
+  def initialize(sender, receiver, transfer=nil)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @transer = transfer
+  end
+
+  def valid?
+  end
+
+  def execute_transaction
+  end
+
+  def reverse_transfer
+  end
+
 end
